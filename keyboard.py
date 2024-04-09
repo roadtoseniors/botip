5160c61d51da9a0e26696af2733ef4291f9a8eba
Road to seniors <152744792+roadtoseniors@users.noreply.github.com>
2024-04-09 12:42:57 +0300

    Add files via upload

---
+++ keyboard.py
@@ -0,0 +1,102 @@
+from aiogram.types import(
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
+from aiogram import Bot, Dispatcher, types
+
+
+
+
+main_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Помощь', callback_data='help'),
+            InlineKeyboardButton(text='Команды', callback_data='command'),
+        ],
+        [
+            InlineKeyboardButton(text='Темы', callback_data='tema')
+
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+
+pravila_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Орфография',callback_data='orfografia'),
+            InlineKeyboardButton(text='Пунктуация', callback_data='pynktyacia')
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+istochnik_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Учебник по русскому', callback_data='istochnik')
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+orfogra_fia = InlineKeyboardMarkup(
+
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Лесика', callback_data='leksika'),
+            InlineKeyboardButton(text='Орфография и приставки', callback_data='orfografia')
+        ],
+        [
+            InlineKeyboardButton(text='Имя существительное', callback_data='syh'),
+            InlineKeyboardButton(text='Имя прилагательное', callback_data='prilagatelnoe')
+        ],
+        [
+            InlineKeyboardButton(text='Имя числительное',callback_data='chisl'),
+            InlineKeyboardButton(text='Глагол', callback_data='glagol')
+        ],
+        [
+            InlineKeyboardButton(text='Местоимение', callback_data='mest'),
+            InlineKeyboardButton(text='Причастие и деепричастие', callback_data='prich')
+        ],
+        [
+            InlineKeyboardButton(text='Наречие' ,callback_data='nar')
+        ]
+    ],
+
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+pynktyaci_a = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Знаки законченности', callback_data='tochka'),
+            InlineKeyboardButton(text='Знаки незаконченности', callback_data='zapatie')
+        ],
+        [
+            InlineKeyboardButton(text='Двойные знаки', callback_data='two'),
+            InlineKeyboardButton(text='Простое предложение', callback_data='prost')
+        ],
+        [
+            InlineKeyboardButton(text='Пунктуация при обращении', callback_data='obrsh'),
+            InlineKeyboardButton(text='Сравнительные обороты', callback_data='')
+        ],
+        [
+            InlineKeyboardButton(text='Прямая речь', callback_data='pram'),
+            InlineKeyboardButton(text='Цитирование', callback_data='citirov')
+        ],
+        [
+            InlineKeyboardButton(text='ССП', callback_data='ssp'),
+            InlineKeyboardButton(text='СПП', callback_data='spp')
+        ],
+        [
+            InlineKeyboardButton(text='СБП', callback_data='sbp'),
+            InlineKeyboardButton(text='Виды синтаксической связи', callback_data='sintaks')
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
---
+++ main.py
@@ -0,0 +1,82 @@
+import asyncio
+import logging
+import aiogram
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import StateFilter
+from aiogram.filters.command import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+import stategroup
+
+import keyboard
+# from stategroup import StepsForm
+
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token='6961486369:AAF14quNlNE8xurAd243y1H6qUQBFQfMc3g')
+dp = Dispatcher()
+
+
+
+@dp.message(Command('start'))
+async def start(message: types.Message):
+    await message.answer(text='Привет это справочник по русскому для студентов КИПФИН. Здесь собраны все изучаемые темы'
+                         ' на первом курсе колледжа',reply_markup=keyboard.main_kb)
+
+@dp.message(Command('command'))
+async def command(message: types.Message):
+    await message.answer(text='Список команд бота:\n'
+                              '/start-сам запуск бота\n'
+                              '/help-для найденных ошибок\n'
+                              '/istochnik-учебник, который всегда под рукой\n'
+                              '/tema-все собранные темы для 1 курса\n'
+                              '/orfografia-все темы раздела орфография\n'
+                              '/pynktyacia-все темы раздела пунктуация\n')
+
+@dp.message(Command('help'))
+async def start(message: types.Message,state: FSMContext):
+    await message.answer("Если вы нашли ошибку в каком-либо разделе, то передайте её сюда, мы все исправим.")
+    await state.set_state(stategroup.HelpForm.Text)
+
+
+@dp.callback_query()
+async def cmd_callback(call: types.CallbackQuery, state: FSMContext):
+    await call.answer()
+    if call.data == 'help':
+        await call.message.answer("Если вы нашли ошибку в каком-либо разделе, то передайте её сюда, мы все исправим.")
+        await state.set_state(stategroup.HelpForm.Text)
+    elif call.data == 'command':
+        await call.message.answer(text='Список команд бота:\n'
+                                       '/start-сам запуск бота\n'
+                                       '/help-для найденных ошибок\n'
+                                       '/istochnik-учебник, который всегда под рукой\n'
+                                       '/tema-все собранные темы для 1 курса')
+    elif call.data == 'istochnik':
+        await call.message.answer(text="https://clck.ru/39fv4H")
+    elif call.data == 'orfografia':
+        await call.message.answer(text='Выберете тему орфографии:', reply_markup=keyboard.orfogra_fia)
+    elif call.data == 'pynktyacia':
+        await call.message.answer(text='Выберете тему пунктуации:', reply_markup=keyboard.pynktyaci_a)
+    elif call.data == 'tema':
+        await call.message.answer(text="Выберете раздел, который вам нужен", reply_markup=keyboard.pravila_kb)
+
+
+@dp.message(Command("istochnik"))
+async def cmd_inline_url(message: types.Message, bot: Bot):
+    # builder = InlineKeyboardBuilder()
+    # builder.row(types.InlineKeyboardButton(
+    #     text="Учебник по русскому", url='https://clck.ru/39fv4H')
+    # )
+    await message.answer(
+        "Источник, где можно удостовериться в правильности информации",
+        reply_markup=keyboard.istochnik_kb
+    )
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
